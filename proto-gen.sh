183f5f810f81e6eb85b2bb2b3b14ce4c9cb3f5c8
Sai Kumar <ksai44576@gmail.com>
2023-02-12 09:52:33 +0530

    Add node stub gen script

--- proto-gen.sh
+++ proto-gen.sh
@@ -6,6 +6,12 @@ if [ "$1" = "go" ]; then
         --go_out=. \
         --go-grpc_out=. \
         protos/*.proto
+elif [ "$1" = "node" ]; then
+    protoc \
+        -I=. \
+        --js_out=import_style=commonjs:./web \
+        --grpc-web_out=import_style=typescript,mode=grpcwebtext:./web \
+        protos/*.proto
 else
     echo "Unknown argument: '$1'"
 fi
